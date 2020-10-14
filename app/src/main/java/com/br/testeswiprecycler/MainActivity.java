052f707e898b1fd77147e942617d18847f1b1093
fernandferreira@hotmail.com <fernandferreira@hotmail.com>
2020-10-14 14:42:53 -0300

    Initial commit

---
+++ app/src/main/java/com/br/testeswiprecycler/MainActivity.java
@@ -0,0 +1,99 @@
+package com.br.testeswiprecycler;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Canvas;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<RecyclerEntity> list;
+    RecyclerAdapter adapter;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        list = new ArrayList<>();
+
+        list.add(new RecyclerEntity("This is the best title", R.drawable.one, false));
+        list.add(new RecyclerEntity("This is the second-best title", R.drawable.two, false));
+        list.add(new RecyclerEntity("The Great Gatsby", R.drawable.three, false));
+        list.add(new RecyclerEntity("Catch-22", R.drawable.one, false));
+        list.add(new RecyclerEntity("I really don't know", R.drawable.two, false));
+        list.add(new RecyclerEntity("What to name this title", R.drawable.three, false));
+        list.add(new RecyclerEntity("This is the best title", R.drawable.one, false));
+        list.add(new RecyclerEntity("This is the second-best title", R.drawable.two, false));
+        list.add(new RecyclerEntity("The Great Gatsby", R.drawable.three, false));
+        list.add(new RecyclerEntity("Catch-22", R.drawable.one, false));
+        list.add(new RecyclerEntity("I really don't know", R.drawable.two, false));
+        list.add(new RecyclerEntity("What to name this title", R.drawable.three, false));
+
+        adapter = new RecyclerAdapter(this, list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        ItemTouchHelper.SimpleCallback touchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            private final ColorDrawable background = new ColorDrawable(getResources().getColor(R.color.background));
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                adapter.showMenu(viewHolder.getAdapterPosition());
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+                View itemView = viewHolder.itemView;
+
+                if (dX > 0) {
+                    background.setBounds(itemView.getLeft(), itemView.getTop(), itemView.getLeft() + ((int) dX), itemView.getBottom());
+                } else if (dX < 0) {
+                    background.setBounds(itemView.getRight() + ((int) dX), itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                } else {
+                    background.setBounds(0, 0, 0, 0);
+                }
+
+                background.draw(c);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(touchHelperCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+        recyclerView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                adapter.closeMenu();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (adapter.isMenuShown()) {
+            adapter.closeMenu();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
